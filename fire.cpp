6de97a5c80fe4067d6ef31824b7da4b119d070d0
CZandona <c.zandona1994@gmail.com>
2019-07-26 08:40:33 +0200

    Fase riscaldamento funzionante

--- fire.cpp
+++ fire.cpp
@@ -4,7 +4,6 @@
  *  Copyright  2017	Luca Geretti
  *
  ****************************************************************************/
-
 /*
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
