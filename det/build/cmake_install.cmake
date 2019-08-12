dd8ee0d731b8ac828fbf63ec066224f0e4cd8236
CZandona <c.zandona1994@gmail.com>
2019-08-12 16:31:51 +0200

    determinismo e non determinismo

--- det/build/cmake_install.cmake
+++ det/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/d/GithubRepo/AriadneGit
+# Install script for directory: /mnt/d/GithubRepo/AriadneGit/det
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/d/GithubRepo/AriadneGit/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/mnt/d/GithubRepo/AriadneGit/det/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- non_det/build/CMakeFiles/fire_system.dir/DependInfo.cmake
+++ non_det/build/CMakeFiles/fire_system.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/mnt/d/GithubRepo/AriadneGit/fire.cpp" "/mnt/d/GithubRepo/AriadneGit/build/CMakeFiles/fire_system.dir/fire.cpp.o"
+  "/mnt/d/GithubRepo/AriadneGit/non_det/fire.cpp" "/mnt/d/GithubRepo/AriadneGit/non_det/build/CMakeFiles/fire_system.dir/fire.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
