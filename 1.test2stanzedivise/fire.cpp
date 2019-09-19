8d700aadcb5396684f099c5b0a6c962befb09434
CZandona <c.zandona1994@gmail.com>
2019-09-19 09:09:10 +0200

    2.teststanzeinsieme
    Modificate formule di raffreddamento
    errore nel main

---
+++ 1.test2stanzedivise/fire.cpp
@@ -0,0 +1,86 @@
+/***************************************************************************
+ *            twowatertanks.cpp
+ *
+ *  Copyright  2017	Luca Geretti
+ *
+ ****************************************************************************/
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "ariadne.hpp"
+#include "hot_room.hpp"
+using namespace Ariadne;
+
+using std::cout; using std::endl;using std::flush;
+
+Int main(Int argc, const char* argv[])
+{
+    Nat evolver_verbosity=get_verbosity(argc,argv);
+
+    // Dichiaro le variabili condivise
+    RealVariable temp("temp"); //temperatura
+    RealVariable t("t"); //tempo
+
+    //automi creati
+    StringVariable heating("heating"); //hot_room
+    
+    //stati iniziali
+    StringConstant startHeating("startHeating");
+    StringConstant end("end"); //unico stato hot_room per riscaldamento
+    
+    
+    //creo il sistema locazione + tempo
+    CompositeHybridAutomaton heating_system=create_heating_system();
+    cout << heating_system << endl;
+
+    // Creato un GeneralHybridEvolver object
+    GeneralHybridEvolver evolver(heating_system);
+    evolver.verbosity = evolver_verbosity;
+
+    // Set the evolution parameters
+    evolver.configuration().set_maximum_enclosure_radius(3.05);
+    evolver.configuration().set_maximum_step_size(0.002);
+
+    // Declare the type to be used for the system evolution
+    typedef GeneralHybridEvolver::OrbitType OrbitType;
+    
+    TaylorSeriesIntegrator integrator(MaximumError(1e-2), Order(3) );
+    evolver.set_integrator(integrator);
+
+    
+    std::cout << "Computing evolution... " << std::flush;
+    Dyadic Cinit_max(1,10u);
+
+    //HybridSet initial_set(hr|heating_s,{temp==3482, t==20});
+    double r=1.0/1024; double Ti=20;
+    Real Tinitmin(Ti+r); Real Tinitmax(Ti+3*r); Real Cinitmin(0+r); 
+    HybridSet initial_set(heating|startHeating,{Tinitmin<=temp<=Tinitmax, 0<=t<=Cinit_max});
+    // ariadne calcola in secondi mentre la formula in minuti quindi se in
+    // 3 min raggiungo 600 gradi 
+    HybridTime evolution_time(3.3270,10);
+    OrbitType orbit = evolver.orbit(initial_set,evolution_time,Semantics::UPPER);
+    std::cout << "done." << std::endl;
+
+    std::cout << "Plotting trajectory... "<<std::flush;
+    // plot fase riscaldamento stanza
+    //Axes2d time_temp_axes(0<=TimeVariable()<=evolution_time.continuous_time(),3482<=temp<=10000);
+    Axes2d time_temp_axes(0<=TimeVariable()<=evolution_time.continuous_time(),20<=temp<=1000);
+    plot("FireHotRoom",time_temp_axes, Colour(0.0,0.5,1.0), orbit);
+
+
+
+    std::cout << "done." << std::endl;
+}
---
+++ 2.test2stanzeinsieme/hot_room2.hpp
@@ -0,0 +1,105 @@
+/***************************************************************************
+ *            tank.hpp
+ *
+ *  Copyright  2017 Luca Geretti
+ *
+ ****************************************************************************/
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "ariadne.hpp"
+
+
+using namespace Ariadne;
+
+inline HybridAutomaton create_heating_system2()
+{
+    RealConstant a("a",735); //costante per end heating
+    RealConstant b("b",8); //costante per end heating
+    RealConstant c("c",1); //costante per end heating
+    RealConstant d("d",0.20_decimal); //costante 20s per end heating
+    RealConstant e("e",0.25_decimal); // costante per start heating
+    RealConstant f("f",154); //costante per start heating
+    RealConstant g("g",625); // costante per coolon 
+    RealConstant timef("timef",1.3051_decimal); // tempo in cui si raggiungono 600 incendio
+    RealConstant i("i",0.2_decimal); // fattore correttivo cooling
+    RealConstant l("l",250); // costante per coolgoon e cooloff
+    RealConstant h("h",6.53_decimal); // costante per coolgoon
+    RealConstant m("m",20); // costante per stopHeating
+    RealConstant n("n",1.5082_decimal); // costante per coolgoon
+    RealConstant o("o",2); // costante per cooloff
+    RealConstant p("p",0.30_decimal); // costante per coolgoon
+    RealConstant q("q",0.90_decimal); // costante per cooloff
+    RealConstant r("r",10);
+    RealConstant s("s",1);
+    RealConstant u("u",0.1_decimal);
+    RealConstant tempf("tempf",600);
+    RealConstant ts("ts",0.0005_decimal); 
+    //RealVariable aperture2("aperture2");
+
+    StringVariable heating2("heating2");
+    StringConstant start("start");
+    StringConstant end("end");
+    StringConstant coolgoon("coolgoon");
+    StringConstant coolon("coolon");
+    StringConstant cooloff("cooloff");
+    StringConstant startHeating("startHeating");
+    StringConstant stopHeating("stopHeating");
+    
+
+
+    // creo le variabili
+    RealVariable temp2("temp2"); //temp2eratura
+    RealVariable t("t"); //tempo
+    RealVariable aperture2("aperture2");
+        
+
+    // Create the room object
+    HybridAutomaton hr2("hr2");
+
+    //dichiarare gli eventi
+    DiscreteEvent must_heating2("must_heating2");
+    DiscreteEvent on2("on2");
+    DiscreteEvent more_hot2("more_hot2");
+    DiscreteEvent too_hot2("too_hot2");
+    DiscreteEvent less_hot2("less_hot2");
+    DiscreteEvent less_less_hot2("less_less_hot2");
+    DiscreteEvent end_cool2("end_cool2");
+
+    
+    // modello del sistema che incrementa la sua temperatura
+    hr2.new_mode( heating2|startHeating, {dot(temp2) = 0} );
+    hr2.new_mode( heating2|start, {dot(temp2) = 154*pow(t,Nat(0.25))+20} );
+    hr2.new_mode( heating2|end, {dot(temp2) = a*((log(b*(t-d)+c))/log(r))+d} );
+    hr2.new_mode( heating2|coolon, {dot(temp2) = tempf+((-g)*(aperture2)*(timef-(t*(-i/aperture2))))});
+    //hr2.new_mode( heating2|coolon, {dot(temp2) = (-g)*(aperture2)*(t-timef*(-i))+tempf});
+    //hr2.new_mode( heating2|coolgoon, {dot(temp2) = (-l)*(aperture2)*(h-timef)*(t-timef*(i))+tempf});
+    hr2.new_mode( heating2|coolgoon, {dot(temp2) = tempf+((-l)*(aperture2)*(h-t)*(timef-(t*(-u/aperture2))))});
+    hr2.new_mode( heating2|cooloff, {dot(temp2) = tempf+((-l)*(aperture2)*(timef-(t*(-s/aperture2))))});
+    hr2.new_mode( heating2|stopHeating, {dot(temp2)= aperture2});
+
+    
+    hr2.new_transition( heating2|startHeating, on2, heating2|start, {next(temp2)=temp2}, t>=ts, EventKind::URGENT );
+    hr2.new_transition( heating2|start, more_hot2, heating2|end, {next(temp2)=temp2}, t>=d, EventKind::URGENT );
+    hr2.new_transition( heating2|end, too_hot2, heating2|coolon, {next(temp2)=temp2}, temp2 >=tempf, EventKind:: URGENT);
+    hr2.new_transition( heating2|coolon, less_hot2, heating2|coolgoon, {next(temp2)=temp2}, t>=timef+p, EventKind::URGENT );
+    hr2.new_transition( heating2|coolgoon, less_less_hot2, heating2|cooloff, {next(temp2)=temp2}, t>=timef+p+q, EventKind::URGENT );
+    hr2.new_transition( heating2|cooloff, end_cool2, heating2|stopHeating, {next(temp2)=temp2}, temp2<=m, EventKind::URGENT );
+    
+    
+    return hr2;
+
+}
